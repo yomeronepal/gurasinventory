9e37258ab5ecd86bda1cf7ea5c9455a43b7faed5
Rojesh Prajapati <yomeronepal7@gmail.com>
2021-01-24 20:07:39 +0545

    static

--- inventorysystem/settings.py
+++ inventorysystem/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '$ca)b+vh%^5#%hhqk+2wgqwjbtg5839%fhvwsa!rb7f88w@t)^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
